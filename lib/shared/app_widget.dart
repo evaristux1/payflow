a98e32e72a4b1aff4ae5d56415e200c696f3896c
evaristux <gabrielevaristovcp@gmail.com>
2021-06-26 09:25:19 -0300

    (develop) - Corrigindo erro na camera

--- lib/shared/app_widget.dart
+++ lib/shared/app_widget.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
 
 import 'themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
 
-  @override
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Pay Flow',
         theme: ThemeData(
+          primarySwatch: Colors.orange,
           primaryColor: AppColors.primary,
         ),
-        home: LoginPage(),
+        initialRoute: "/splash",
         routes: {
+          "/splash": (context) => SplashPage(),
           "/home": (context) => HomePage(),
           "/login": (context) => LoginPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage(),
+          "/insert_boleto": (context) => InsertBoletoPage(),
         });
   }
 }
